fb6bb1542259ba8f654fee9852f88312e6d9b945
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-10-03 14:08:00 +0700

    edit index.html file khanhtq 1st

--- index.php
+++ index.php
@@ -1,25 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-	<link rel="stylesheet" href="normalize.css">
-	<meta charset="utf-8">
-	<title>My web page</title>
-	<link rel="stylesheet" href="./assets/css/index.css" />
-	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-
-	<script src="./assets/js//index.js"></script>
-</head>
-
-<body>
-	<?php
-	# Include header
-	require __DIR__ . '/modules/partials/header.php';
-	# Include main contain
-	require __DIR__ . '/modules/home.php';
-	# Include footer
-	require __DIR__ . '/modules/partials/footer.php';
-	?>
-</body>
-
-</html>
+<?php
+# Include header
+require __DIR__ . '/modules/partials/header.php';
+# Include main contain
+require __DIR__ . '/modules/home.php';
+# Include footer
+require __DIR__ . '/modules/partials/footer.php';
