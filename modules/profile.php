4ab0b5b497ed4028c485e9c6fec957bdc56fca10
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-10-10 16:50:31 +0700

    homework 4 function show information user by khanhtq

--- modules/profile.php
+++ modules/profile.php
@@ -4,6 +4,7 @@
         <h3>My profile.</h3>
 
         <!-- Hiển thị nội dung, sử dụng $user đã có ở file header.php -->
+        <!-- homework 4 -->
         <p><strong>ID: </strong><?php echo $user['id']; ?></p>
         <p><strong>Fullname: </strong><?php echo $user['fullname']; ?></p>
         <p><strong>Username: </strong><?php echo $user['username']; ?></p>
