3e8dbe7a8b4a6c62eba4121b667769aa3a6a1f13
khanhtruong92 <khanhtq@pascaliaasia.com>
2022-10-10 15:04:31 +0700

    logout homework 1 2nd by khanhtq

--- modules/partials/header.php
+++ modules/partials/header.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 // Define an array to contain page titles
 $pageTitles = array(
 	'home' => 'Home',
 	'profile' => 'My Profile',
 	'register' => 'Register',
-	'login' => 'Login', 
+	'login' => 'Login',
 	'logout' => 'Logout' // Thêm 1 dòng
 );
 // Get page title depend on what is using module
@@ -34,22 +34,25 @@ $fullname = $user ? $user['fullname'] : 'Guest';
 
 <!DOCTYPE html>
 <html lang="en">
-  <head><link rel="stylesheet" href="normalize.css">
+
+<head>
+	<link rel="stylesheet" href="normalize.css">
 	<meta charset="utf-8">
 	<title><?php echo $pageTitle; ?></title>
 	<link rel="stylesheet" href="./assets/css/index.css" />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	
+
 	<script src="./assets/js//index.js"></script>
-  </head>
-  <body>
-	  <!-- The Header -->
-	  <header> 
+</head>
+
+<body>
+	<!-- The Header -->
+	<header>
 		<div>
 			<h4>The logo <?php echo $_SESSION['login_user_id']; ?></h4>
 		</div>
 		<div>
-			  <h2 class="slogan">The header slogan</h2>
+			<h2 class="slogan">The header slogan</h2>
 		</div>
 		<div id="form">
 			<ul>
@@ -57,14 +60,15 @@ $fullname = $user ? $user['fullname'] : 'Guest';
 				<!-- <li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li> -->
 				<?php if (!$user) { ?>
 					<li><a href="javascript:void(0)" onclick="showLoginForm()">Login</a></li>
-					<?php } else { ?>
+				<?php } else { ?>
+					<!-- Chuyển hướng thông tin vào file logout.php để xử lý -->
 					<li><a href="./index.php?m=logout">Logout</a></li>
 				<?php } ?>
 			</ul>
-			
+
 			<form id="login" action="./index.php?m=login" method="post">
 				<input type="text" name="username" placeholder="User name" />
-				<input type="password" name="password" placeholder="Password"/>
+				<input type="password" name="password" placeholder="Password" />
 				<label><input type="checkbox" name="rememberUsername" />Remember user name </label>
 				<button type="submit" name="Login">Login</button>
 			</form>
@@ -73,14 +77,13 @@ $fullname = $user ? $user['fullname'] : 'Guest';
 				<i class="material-icons">search</i>
 			</form>
 		</div>
-	  </header>
-	  
-	  <!-- The menu -->
-	  <nav>
-		  <ul>
-			  <li><a href="./index.php">Home</a></li>
-			  <li><a href="./index.php?m=register">Register</a></li>
-			  <li><a href="./index.php?m=profile">My Profile</a></li>
-		  </ul>
-	  </nav>
-	  
+	</header>
+
+	<!-- The menu -->
+	<nav>
+		<ul>
+			<li><a href="./index.php">Home</a></li>
+			<li><a href="./index.php?m=register">Register</a></li>
+			<li><a href="./index.php?m=profile">My Profile</a></li>
+		</ul>
+	</nav>
